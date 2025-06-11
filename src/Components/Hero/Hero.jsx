69181d09767cc80d6078aba2be032302eee883b7
andy heggs <andy@heggs.net>
2025-06-11 14:14:55 +0100

    Hero classes updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,10 +6,18 @@ import LogoSolo from '../../assets/LogoSolo.svg'
 const Hero = () => {
   return (
     <div id='hero' className='hero'>
-      <img src={LogoSolo} alt=""/>
-      <h1>Andy Heggs</h1>
-      <h2>Full stack developer & data scientist</h2>
-      <p>Capitalising data and building intelligent business solutions.</p>
+      <div className='hero-logo'>
+        <img src={LogoSolo} alt=""/>
+      </div>
+      <div className='hero-myname'>
+        <h1>Andy Heggs</h1>
+      </div>
+      <div className='hero-techtitle'>
+        <h2>Full stack developer & data scientist</h2>
+      </div>
+      <div className='hero-tagline'>
+        <p>Capitalising data and building intelligent business solutions.</p>
+      </div>
     </div>
   )
 }
